2b8c7683c785118ea0e6cbd16e62accedebbc106
Mac Owner <macowner@Macs-MacBook-Pro.local>
2014-01-25 21:31:04 -0600

    added instrutions to rake task

--- app/controllers/checkins_controller.rb
+++ app/controllers/checkins_controller.rb
@@ -35,7 +35,6 @@ class CheckinsController < ApplicationController
   #     driver.execute_script "window.onbeforeunload = function(e){};"
   #     driver.quit
   # end
-
   def new
     # driver = Selenium::WebDriver.for :firefox
     @brands = Brand.all
@@ -52,21 +51,23 @@ class CheckinsController < ApplicationController
       # driver.quit
   end
 
-  def perform
-    # driver = Selenium::WebDriver.for :firefox
-    @brands = Brand.all
-    checkin = Time.now
+end
 
-    @brands.each do |brand|
-      facebook_link = brand.facebook_link
-      youtubeteaser_link = brand.youtubeteaser_link
-      topsy_link = brand.topsy_link
-      Checkin.data_lookup(topsy_link, youtubeteaser_link, facebook_link, brand.id, checkin)
-    end
-    render :new
-      # driver.execute_script "window.onbeforeunload = function(e){};"
-      # driver.quit
-  end
+  # def perform
+  #   # driver = Selenium::WebDriver.for :firefox
+  #   @brands = Brand.all
+  #   checkin = Time.now
+
+  #   @brands.each do |brand|
+  #     facebook_link = brand.facebook_link
+  #     youtubeteaser_link = brand.youtubeteaser_link
+  #     topsy_link = brand.topsy_link
+  #     Checkin.data_lookup(topsy_link, youtubeteaser_link, facebook_link, brand.id, checkin)
+  #   end
+  #   # render :new
+  #     # driver.execute_script "window.onbeforeunload = function(e){};"
+  #     # driver.quit
+  # end
 
 end
 
--- lib/tasks/scheduler.rake
+++ lib/tasks/scheduler.rake
@@ -1,16 +1,14 @@
 desc "This task is called by the Heroku scheduler add-on"
 task :this_task => :environment do
   puts "Running checkins..."
-  Checkin.perform
-  # curl -L -s http://localhost:3000/new/?refresh
-  # @brands = Brand.first(2)
-    # checkin = Time.now
-# puts checkin
-  #   @brands.each do |brand|
-  #     facebook_link = brand.facebook_link
-  #     youtubeteaser_link = brand.youtubeteaser_link
-  #     topsy_link = brand.topsy_link
-      # Checkin.data_lookup( topsy_link, youtubeteaser_link, facebook_link, brand.id, checkin)
+  @brands = Brand.all
+    checkin = Time.now
+
+    @brands.each do |brand|
+      facebook_link = brand.facebook_link
+      youtubeteaser_link = brand.youtubeteaser_link
+      topsy_link = brand.topsy_link
+      Checkin.data_lookup(topsy_link, youtubeteaser_link, facebook_link, brand.id, checkin)
   puts "done."
     # end
 end
