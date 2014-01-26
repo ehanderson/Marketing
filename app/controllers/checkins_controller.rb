0c2cb8355a27df3e40e9fa83499b728eaf82c468
Mac Owner <macowner@Macs-MacBook-Pro.local>
2014-01-25 21:00:10 -0600

    heroku scheduler set'

--- app/controllers/checkins_controller.rb
+++ app/controllers/checkins_controller.rb
@@ -20,7 +20,7 @@ class CheckinsController < ApplicationController
     redirect_to :show_checkin_data
   end
 
-  # def perform
+  # def new
   #   driver = Selenium::WebDriver.for :firefox
   #   @brands = Brand.all
   #   checkin = Time.now
@@ -37,6 +37,22 @@ class CheckinsController < ApplicationController
   # end
 
   def new
+    # driver = Selenium::WebDriver.for :firefox
+    @brands = Brand.all
+    checkin = Time.now
+
+    @brands.each do |brand|
+      facebook_link = brand.facebook_link
+      youtubeteaser_link = brand.youtubeteaser_link
+      topsy_link = brand.topsy_link
+      Checkin.data_lookup(topsy_link, youtubeteaser_link, facebook_link, brand.id, checkin)
+    end
+    render :new
+      # driver.execute_script "window.onbeforeunload = function(e){};"
+      # driver.quit
+  end
+
+  def perform
     # driver = Selenium::WebDriver.for :firefox
     @brands = Brand.all
     checkin = Time.now
